ce0207e8554bbbfe3164127f139dfcf320023ae2
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-05-21 11:43:32 +0300

    update page

--- my-test-project/src/index.js
+++ my-test-project/src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Ann-Yurchuk/my-test-project">
+    <BrowserRouter basename="/Ann-Yurchuk/my-test-project/">
       <BackBtnToTop />
       <App />
     </BrowserRouter>
