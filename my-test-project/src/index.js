233125f70daac8dd0ab92fee5234736176f61f60
Ann-Yurchuk <105579383+Ann-Yurchuk@users.noreply.github.com>
2023-05-21 10:39:20 +0300

    add files for work

---
+++ my-test-project/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { App } from 'App';
+import BackBtnToTop from 'utils/buttonBackToTop';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/Ann-Yurchuk/my-test-project/">
+      <BackBtnToTop />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
